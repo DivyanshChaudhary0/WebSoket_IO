a1fac33dbab2e16bfc6b4c528b5806c0d03c3010
Divyansh chaudhary <hypr123890@gmail.com>
2025-03-29 11:59:00 +0530

    server changed

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = require("./src/app")
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 io.on('connection', socket => {
+    socket.on("message",function(data){
+        console.log(data);
+        io.emit("chat-message", data)
+    })
     console.log("user connected");
 });
 
